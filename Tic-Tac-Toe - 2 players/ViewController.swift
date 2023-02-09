d1fb9c509196a15409b503cca738293fa6f37ddd
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-02-09 11:50:43 +0100

    Initial Commit

---
+++ Tic-Tac-Toe - 2 players/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe - 2 players
+//
+//  Created by David Třešňák on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
