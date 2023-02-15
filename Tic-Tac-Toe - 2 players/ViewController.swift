829b3a0ce888e01febff37f6ae645f5cac65ca6b
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-02-15 12:53:51 +0100

    V 1.0

--- Tic-Tac-Toe - 2 players/ViewController.swift
+++ Tic-Tac-Toe - 2 players/ViewController.swift
@@ -9,25 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var winningCombos = [["a1", "b1", "c1"],  //model
+    let winningCombos = [["a1", "b1", "c1"],
                          ["a2", "b2", "c2"],
                          ["a3", "b3", "c3"],
                          ["a1", "a2", "a3"],
                          ["b1", "b2", "b3"],
                          ["c1", "c2", "c3"],
                          ["a1", "b2", "c3"],
-                         ["a3", "b2", "c1"]   ]
+                         ["a3", "b2", "c1"]]
     
-    
-    var currentPlayer = "circle" // model
-    var winner: String?          // model
+    lazy var allButtons = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+    var currentPlayer = "circle"
+    var winner: String?
     var xPlayerClaimed: Set = ["", ""]
     var oPlayerClaimed: Set = ["", ""]
     var oPlayerScore = 0
     var xPlayerScore = 0
     
-    
-    @IBOutlet weak var a1: UIButton! //Controller
+    @IBOutlet weak var a1: UIButton!
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var c1: UIButton!
     @IBOutlet weak var a2: UIButton!
@@ -40,120 +39,83 @@ class ViewController: UIViewController {
     @IBOutlet weak var xPlayerScoreLabel: UILabel!
     @IBOutlet weak var playerWinsLabel: UILabel!
     
-    
-    override func viewDidLoad() { //Controller
+    override func viewDidLoad() {
         super.viewDidLoad()
         playerWinsLabel.textColor = .clear
     }
     
-    
-    
-    @IBAction func resetButtonPressed(_ sender: UIButton) { //Controller
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
         resetGame()
     }
     
-    @IBAction func fieldButtonPressed(_ sender: UIButton) { //Controller
+    @IBAction func fieldButtonPressed(_ sender: UIButton) {
         if sender.currentImage == nil {
             sender.setImage(UIImage(systemName: currentPlayer), for: .normal)
             if currentPlayer == "circle" {
-                oPlayerClaimed.insert(sender.accessibilityLabel ?? "") //Model
+                oPlayerClaimed.insert(sender.accessibilityLabel ?? "")
                 checkWinner()
-                
-                currentPlayer = "xmark" // Model
-                
+                currentPlayer = "xmark"
             } else if currentPlayer == "xmark" {
-                xPlayerClaimed.insert(sender.accessibilityLabel ?? "") //Model
+                xPlayerClaimed.insert(sender.accessibilityLabel ?? "")
                 checkWinner()
-                currentPlayer = "circle" // Model
+                currentPlayer = "circle"
             }
         }
-        
-        
-        func checkWinner() { // Model
-            if currentPlayer == "circle" {
-                for x in 0...7 {
-                    if oPlayerClaimed.isSuperset(of: winningCombos[x]) {
-                        winner = "circle"
-                    }
-                }
-            } else if currentPlayer == "xmark" {
-                for x in 0...7 {
-                    if xPlayerClaimed.isSuperset(of: winningCombos[x]) {
-                        winner = "xmark"
-                    }
-                }
-            }
-            
-            if a1.currentImage != nil && a2.currentImage != nil && a3.currentImage != nil && b1.currentImage != nil && b2.currentImage != nil && b3.currentImage != nil && c1.currentImage != nil && c2.currentImage != nil && c3.currentImage != nil && winner == nil {
-                winner = "draw"
-            }
-            
-            
-            if winner != nil {
-                if winner == "xmark" {
-                    playerWinsLabel.text = "x Player Wins!"
-                    xPlayerScore += 1
-                } else if winner == "circle" {
-                    playerWinsLabel.text = "o Player Wins!"
-                    oPlayerScore += 1
-                } else if winner == "draw" {
-                    playerWinsLabel.text = "It's a draw!"
-                }
-                oPlayerScoreLabel.text = String(oPlayerScore)
-                xPlayerScoreLabel.text = String(xPlayerScore)
-                playerWinsLabel.textColor = .systemBlue
-                
-                enableButtons(false)
-
-
-            }
-            
-        }
-        
     }
-    func resetGame() { // Model
-        enableButtons(true)
-        
+    
+    func resetGame() {
+        enableButtons(set: true)
         playerWinsLabel.textColor = .clear
-        currentPlayer = "circle"
-        winner = nil
         oPlayerClaimed.removeAll()
         xPlayerClaimed.removeAll()
+        currentPlayer = "circle"
+        winner = nil
         
-        print("resetGame pressed")
+        for button in allButtons {
+            button!.configuration?.image = nil
+            button!.setImage(nil, for: .normal)
+        }
+    }
 
-        a1.configuration?.image = nil
-        a1.setImage(nil, for: .normal)
-        a2.configuration?.image = nil
-        a2.setImage(nil, for: .normal)
-        a3.configuration?.image = nil
-        a3.setImage(nil, for: .normal)
-        b1.configuration?.image = nil
-        b1.setImage(nil, for: .normal)
-        b2.configuration?.image = nil
-        b2.setImage(nil, for: .normal)
-        b3.configuration?.image = nil
-        b3.setImage(nil, for: .normal)
-        c1.configuration?.image = nil
-        c1.setImage(nil, for: .normal)
-        c2.configuration?.image = nil
-        c2.setImage(nil, for: .normal)
-        c3.configuration?.image = nil
-        c3.setImage(nil, for: .normal)
-
+    func checkWinner() {
+        if currentPlayer == "circle" {
+            for x in 0...7 {
+                if oPlayerClaimed.isSuperset(of: winningCombos[x]) {
+                    winner = "circle"
+                }
+            }
+        } else if currentPlayer == "xmark" {
+            for x in 0...7 {
+                if xPlayerClaimed.isSuperset(of: winningCombos[x]) {
+                    winner = "xmark"
+                }
+            }
+        } else if a1.currentImage != nil && a2.currentImage != nil && a3.currentImage != nil && b1.currentImage != nil && b2.currentImage != nil && b3.currentImage != nil && c1.currentImage != nil && c2.currentImage != nil && c3.currentImage != nil && winner == nil {
+            winner = "draw"
+        }
+        
+        
+        if winner != nil {
+            if winner == "xmark" {
+                playerWinsLabel.text = "x Player Wins!"
+                xPlayerScore += 1
+            } else if winner == "circle" {
+                playerWinsLabel.text = "o Player Wins!"
+                oPlayerScore += 1
+            } else if winner == "draw" {
+                playerWinsLabel.text = "It's a draw!"
+            }
+            oPlayerScoreLabel.text = String(oPlayerScore)
+            xPlayerScoreLabel.text = String(xPlayerScore)
+            playerWinsLabel.textColor = .systemBlue
+            enableButtons(set: false)
+        }
     }
     
-    func enableButtons(_ buttonStatus: Bool) { // Model
-        a1.isEnabled = buttonStatus
-        a2.isEnabled = buttonStatus
-        a3.isEnabled = buttonStatus
-        b1.isEnabled = buttonStatus
-        b2.isEnabled = buttonStatus
-        b3.isEnabled = buttonStatus
-        c1.isEnabled = buttonStatus
-        c2.isEnabled = buttonStatus
-        c3.isEnabled = buttonStatus
+    func enableButtons(set buttonStatus: Bool) {
+        for button in allButtons {
+            button!.isEnabled = buttonStatus
+        }
     }
-    
 }
 
