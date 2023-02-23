d7348204f099c620db4a7bcdfc5173273701f325
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-02-23 11:06:44 +0100

    V 1.2
    conforming to MVC

--- Tic-Tac-Toe - 2 players/Controller/ViewController.swift
+++ Tic-Tac-Toe - 2 players/Controller/ViewController.swift
@@ -9,23 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let winningCombos = [["a1", "b1", "c1"], 
-                         ["a2", "b2", "c2"],
-                         ["a3", "b3", "c3"],
-                         ["a1", "a2", "a3"],
-                         ["b1", "b2", "b3"],
-                         ["c1", "c2", "c3"],
-                         ["a1", "b2", "c3"],
-                         ["a3", "b2", "c1"]]
-
- 
+    let gameBrain = GameBrain()
     lazy var allButtons = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
-    var currentPlayer = constants.circle
-    var winner: String?
-    var xPlayerClaimed: Set = ["", ""]
-    var oPlayerClaimed: Set = ["", ""]
-    var oPlayerScore = 0
-    var xPlayerScore = 0
     
     @IBOutlet weak var a1: UIButton!
     @IBOutlet weak var b1: UIButton!
@@ -39,89 +24,82 @@ class ViewController: UIViewController {
     @IBOutlet weak var oPlayerScoreLabel: UILabel!
     @IBOutlet weak var xPlayerScoreLabel: UILabel!
     @IBOutlet weak var playerWinsLabel: UILabel!
-    let allFields = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         playerWinsLabel.textColor = .clear
-        oPlayerScoreLabel.text = String(oPlayerScore)
-        xPlayerScoreLabel.text = String(xPlayerScore)
+        gameBrain.addObserver(self, forKeyPath: "winner", options: .new, context: nil)
     }
     
     @IBAction func resetGamePressed(_ sender: UIBarButtonItem) {
-        resetGame()
+        gameBrain.resetGame()
+        resetGameUI()
     }
     
     @IBAction func fieldButtonPressed(_ sender: UIButton) {
-        if sender.currentImage == nil {
-            sender.setImage(UIImage(systemName: currentPlayer), for: .normal)
-            if currentPlayer == constants.circle {
-                oPlayerClaimed.insert(sender.accessibilityLabel ?? "")
-                checkWinner()
-                currentPlayer = constants.xmark
-            } else if currentPlayer == constants.xmark {
-                xPlayerClaimed.insert(sender.accessibilityLabel ?? "")
-                checkWinner()
-                currentPlayer = constants.circle
+        // Controller zkontroluje že pole není obsazené
+        // Není obsazený = když je aktuální hráč O tak:
+        // přidá hráči pole do jeho Claimed
+        // spustí checkwinner
+        // změní aktuálního hráče na X
+        
+        //Controller
+        
+        
+        if sender.currentImage == nil { //MODEL
+            sender.setImage(UIImage(systemName: gameBrain.currentPlayer), for: .normal)
+            if gameBrain.currentPlayer == constants.circle {
+                gameBrain.oPlayerClaimed.insert(sender.accessibilityLabel ?? "")
+                gameBrain.checkWinner()
+                gameBrain.currentPlayer = constants.xmark //MODEL
+                
+                
+            } else if gameBrain.currentPlayer == constants.xmark { //MODEL
+                gameBrain.xPlayerClaimed.insert(sender.accessibilityLabel ?? "")
+                gameBrain.checkWinner()
+                gameBrain.currentPlayer = constants.circle //MODEL
             }
         }
     }
     
-    func resetGame() {
+    func resetGameUI() {
         enableButtons(set: true)
         playerWinsLabel.textColor = .clear
-        oPlayerClaimed.removeAll()
-        xPlayerClaimed.removeAll()
-        currentPlayer = constants.circle
-        winner = nil
-        
         for button in allButtons {
             button!.configuration?.image = nil
             button!.setImage(nil, for: .normal)
         }
     }
-
-    func checkWinner() { //MODEL
-        let claimedFields: Set = xPlayerClaimed.union(oPlayerClaimed)
-        
-        if claimedFields.isSuperset(of: allFields) && winner == nil {
-            winner = constants.draw
-        } else if currentPlayer == constants.circle {
-            for x in 0...7 {
-                if oPlayerClaimed.isSuperset(of: winningCombos[x]) {
-                    winner = constants.circle
-                }
-            }
-        } else if currentPlayer == constants.xmark {
-            for x in 0...7 {
-                if xPlayerClaimed.isSuperset(of: winningCombos[x]) {
-                    winner = constants.xmark
-                }
-            }
-        }
-        
-        if winner != nil {
-            if winner == constants.xmark {
-                playerWinsLabel.text = "x Player Wins!"
-                xPlayerScore += 1
-            } else if winner == constants.circle {
-                playerWinsLabel.text = "o Player Wins!"
-                oPlayerScore += 1
-            } else if winner == constants.draw {
-                playerWinsLabel.text = "It's a draw!"
-            }
-            print(oPlayerScore, xPlayerScore)
-            oPlayerScoreLabel.text = String(oPlayerScore)
-            xPlayerScoreLabel.text = String(xPlayerScore)
-            playerWinsLabel.textColor = .systemBlue
-            enableButtons(set: false)
-        }
-    }
+    
     func enableButtons(set buttonStatus: Bool) {
         for button in allButtons {
             button!.isEnabled = buttonStatus
         }
     }
+    
+    //MARK: observer that updates UI whenever gameBrain.winner changes from none:
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "winner" {
+            if let winner = change?[.newKey] as? String {
+                playerWinsLabel.text = winner
+                playerWinsLabel.textColor = .blue
+                if winner != "none" {
+                    if winner == constants.xmark {
+                        playerWinsLabel.text = "x Player Wins!"
+                    } else if winner == constants.circle {
+                        playerWinsLabel.text = "o Player Wins!"
+                    } else if winner == constants.draw {
+                        playerWinsLabel.text = "It's a draw!"
+                    }
+                    oPlayerScoreLabel.text = String(gameBrain.oPlayerScore)
+                    xPlayerScoreLabel.text = String(gameBrain.xPlayerScore)
+                    playerWinsLabel.textColor = .systemBlue
+                    enableButtons(set: false)
+                }
+            }
+        }
+    }
+    
 }
 
--- Tic-Tac-Toe - 2 players/Model/GameBrain.swift
+++ Tic-Tac-Toe - 2 players/Model/GameBrain.swift
@@ -6,3 +6,56 @@
 //
 
 import UIKit
+
+class GameBrain: UIViewController {
+    
+    let allFields = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+    let winningFieldCombos = [["a1", "b1", "c1"],
+                              ["a2", "b2", "c2"],
+                              ["a3", "b3", "c3"],
+                              ["a1", "a2", "a3"],
+                              ["b1", "b2", "b3"],
+                              ["c1", "c2", "c3"],
+                              ["a1", "b2", "c3"],
+                              ["a3", "b2", "c1"]]
+    var xPlayerClaimed: Set = ["", ""]
+    var oPlayerClaimed: Set = ["", ""]
+    var currentPlayer = constants.circle
+    @objc dynamic var winner = "none"
+    var oPlayerScore = 0
+    var xPlayerScore = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    func resetGame() {
+        oPlayerClaimed.removeAll()
+        xPlayerClaimed.removeAll()
+        currentPlayer = constants.circle
+        winner = "none"
+    }
+    
+    func checkWinner() {
+        let claimedFields: Set = xPlayerClaimed.union(oPlayerClaimed)
+        if claimedFields.isSuperset(of: allFields) && winner == "none" {
+            winner = constants.draw
+        } else if currentPlayer == constants.circle {
+            for x in 0...7 {
+                if oPlayerClaimed.isSuperset(of: winningFieldCombos[x]) {
+                    oPlayerScore += 1
+                    winner = constants.circle
+                }
+            }
+        } else if currentPlayer == constants.xmark {
+            for x in 0...7 {
+                if xPlayerClaimed.isSuperset(of: winningFieldCombos[x]) {
+                    xPlayerScore += 1
+                    winner = constants.xmark
+                }
+            }
+        }
+    }
+}
